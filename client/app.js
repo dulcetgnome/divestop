5e3d556f1fb9943bb17ce59aac9c5f0bd01c128c
rfenolio <mordoc@gmail.com>
2015-09-24 16:04:28 -0700

    (feature) implement map and markers

--- client/app.js
+++ client/app.js
@@ -1,19 +1,17 @@
-angular.module('divestop', ['uiGmapgoogle-maps'])
-  .controller("MapController", function($scope) {
-    $scope.map = {
-      center: {latitude: site.location[0],
-              longitude: site.location[1]
-      },
+angular.module('divestop', ['ngMap'])
+  .controller("OurMapController", function($scope) {
+    $scope.ourmap = {
+      center: site.location,
       zoom: 8
-    }
+    };
   });
 
 var site = {
   name: "Akase",
-  location: [33.688895, 130.295930],
+  location: '33.688895, 130.295930',
   maxDepth: 12,
-  description: "A lovely cove that is perfect for beginner divers./
-    It starts out shallow with pool like conditions good for training, /
+  description: "A lovely cove that is perfect for beginner divers.\
+    It starts out shallow with pool like conditions good for training, \
     and then allows for divers to swim out and enjoy a relaxed dive",
   aquaticLife: ["fish"],
   features: ['coral', 'shallow', 'calm'],
