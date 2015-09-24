5649c2e995a64df7761b1d7388d3638a0d9fbaf3
rfenolio <mordoc@gmail.com>
2015-09-24 12:00:34 -0700

    create basic framework for HTML site for the client

---
+++ client/stub.js
@@ -0,0 +1,12 @@
+// stub data for a site
+var site = {
+  name: "Akase",
+  location: [33.688895, 130.295930],
+  maxDepth: 12,
+  description: "A lovely cove that is perfect for beginner divers./
+    It starts out shallow with pool like conditions good for training, /
+    and then allows for divers to swim out and enjoy a relaxed dive",
+  aquaticLife: ["fish"],
+  features: ['coral', 'shallow', 'calm'],
+  pictures: ['url1', 'url2']
+}
