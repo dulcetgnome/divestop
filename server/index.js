8d537d03070117c965ed2962efa2889d1ccdc28f
Corey Roy <corey.j.roy@gmail.com>
2015-09-24 15:04:47 -0700

    Amend postgres connection criteria

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 var pg = require('pg');
-var connectionString = 'postgres://corey_air:!Gcc-104@localhost/corey_air';
+var connectionString = 'postgres://jvswcsqzyyhjsq:ESK6iNDuI5K9BsOLIF9_OBn8MD@ec2-54-227-249-165.compute-1.amazonaws.com:5432/d90r1nq5eejjap';
+
+
 
 pg.connect(connectionString, function(err, client, done) {
   if (err) {
