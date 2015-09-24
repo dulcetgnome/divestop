89647deb21292c019c078c7d2a0474173f866be6
Corey Roy <corey.j.roy@gmail.com>
2015-09-24 11:58:21 -0700

    Add conde that achieves a Postgresql connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,16 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+var pg = require('pg');
+var connectionString = 'postgres://corey_air:!Gcc-104@localhost/corey_air';
+
+pg.connect(connectionString, function(err, client, done) {
+  if (err) {
+    console.log('error fetching client from pool', err);
+  }
+  console.log('connected to database');
+});
+
 app.get('/', function(req, res) {
   res.send(200, 'Hello world!');
 });
