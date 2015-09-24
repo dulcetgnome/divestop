f907cf913517c9e0cdbafa01310c20c919a08121
Garrett Maring <garrett.maring@gmail.com>
2015-09-24 15:24:35 -0700

    Correct postgres/heroku URL in server.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 var pg = require('pg');
-var connectionString = 'postgres://jvswcsqzyyhjsq:ESK6iNDuI5K9BsOLIF9_OBn8MD@ec2-54-227-249-165.compute-1.amazonaws.com:5432/d90r1nq5eejjap';
+var connectionString = 'postgres://wzypzgrifbmmqn:J3vMBj5wK5nb-Z4dFjcfwqSXZL@ec2-54-243-149-147.compute-1.amazonaws.com:5432/d3dvhl55btercp';
 
 
 
