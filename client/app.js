796aad8b64871a7f7fddac35a766dfd43145e45e
rfenolio <mordoc@gmail.com>
2015-09-24 20:48:46 -0700

    change dive site view when you click on a marker

--- client/app.js
+++ client/app.js
@@ -1,26 +1,64 @@
 angular.module('divestop', ['ngMap'])
   .controller("OurMapController", function($scope) {
+    console.log(sites[0]);
+    $scope.showSiteInfo = false;
+    $scope.currentSite = sites[0];
+      // {name: "",
+      // maxDepth: undefined,
+      // description: "",
+      // aquaticLife: [],
+      // features: [],
+      // pictures: []
+    // }
     $scope.$on("mapInitialized", function(e, map) {
       addMarkers(sites, map);
     })
+
+    var addMarkers = function(sites, map){
+      for (var i = 0; i < sites.length; i++) {
+        var site = sites[i];
+        var marker = new google.maps.Marker({
+          position: site.coordinates,
+          map: map,
+          title: site.name,
+          diveSite: site
+        })
+        marker.addListener('click', function(){
+          debugger;
+          // if siteInfo view
+            // remove view
+          // create new view with clicked info
+          $scope.showSiteInfo = true;
+          $scope.currentSite.name = this.diveSite.name;
+          $scope.currentSite.description = this.diveSite.description;
+          $scope.currentSite.aquaticLife = this.diveSite.aquaticLife;
+          $scope.currentSite.maxDepth = this.diveSite.maxDepth;
+          $scope.currentSite.pictures = this.diveSite.pictures;
+          $scope.$apply();
+          console.log(this)}
+        );
+      }
+
+      var geocoder = new google.maps.Geocoder();
+
+      function geocodeAddress() {
+        var address = $scope.searchBoxText;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === google.maps.GeocoderStatus.OK) {
+            $scope.map.setCenter(results[0].geometry.location);
+            console.log('in geocodeAddress');
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+      }
+    };
+
+    $scope.centerMap = function() {
+
+    };
   });
 
-
-var addMarkers = function(sites, map){
-  for (var i = 0; i < sites.length; i++) {
-    console.log("hitting this")
-    var site = sites[i];
-    var marker = new google.maps.Marker({
-      position: site.coordinates,
-      map: map,
-      title: site.name,
-      divesite: site
-    })
-    marker.addListener('click', function(){
-      console.log(this)}
-    );
-  }
-};
 
 var sites = [{
   name: "Akase",
