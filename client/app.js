bfe19e76b88f84ada22afd1c272e2aedb0733d61
rfenolio <mordoc@gmail.com>
2015-09-24 19:46:43 -0700

    (feature) add multiple markers and add divesite information to each marker

--- client/app.js
+++ client/app.js
@@ -1,24 +1,30 @@
 angular.module('divestop', ['ngMap'])
   .controller("OurMapController", function($scope) {
-    $scope.ourmap = {
-      name: "Akase Beach",
-      center: site.location,
-      zoom: 8
-    };
-
-    $scope.showCustomMarker = function() {
-      debugger;
-      $scope.map.customMarkers.foo.setVisible(true);
-    };
-
-    $scope.closeCustomMarker = function() {
-      $scope.map.customMarkers.foo.setVisible(false);
-    };
+    $scope.$on("mapInitialized", function(e, map) {
+      addMarkers(sites, map);
+    })
   });
 
-var site = {
+
+var addMarkers = function(sites, map){
+  for (var i = 0; i < sites.length; i++) {
+    console.log("hitting this")
+    var site = sites[i];
+    var marker = new google.maps.Marker({
+      position: site.coordinates,
+      map: map,
+      title: site.name,
+      divesite: site
+    })
+    marker.addListener('click', function(){
+      console.log(this)}
+    );
+  }
+};
+
+var sites = [{
   name: "Akase",
-  location: '33.688895, 130.295930',
+  coordinates: {lat:33.688895, lng:130.295930},
   maxDepth: 12,
   description: "A lovely cove that is perfect for beginner divers.\
     It starts out shallow with pool like conditions good for training, \
@@ -26,4 +32,37 @@ var site = {
   aquaticLife: ["fish"],
   features: ['coral', 'shallow', 'calm'],
   pictures: ['url1', 'url2']
-}
+},
+{
+  name: "Shirose",
+  coordinates: {lat:33.686958, lng:130.305055},
+  maxDepth: 12,
+  description: "A lovely cove that is perfect for beginner divers.\
+    It starts out shallow with pool like conditions good for training, \
+    and then allows for divers to swim out and enjoy a relaxed dive",
+  aquaticLife: ["fish"],
+  features: ['coral', 'shallow', 'calm'],
+  pictures: ['url1', 'url2']
+},
+{
+  name: "Kurose",
+  coordinates: {lat:33.689244, lng:130.300034},
+  maxDepth: 12,
+  description: "A lovely cove that is perfect for beginner divers.\
+    It starts out shallow with pool like conditions good for training, \
+    and then allows for divers to swim out and enjoy a relaxed dive",
+  aquaticLife: ["fish"],
+  features: ['coral', 'shallow', 'calm'],
+  pictures: ['url1', 'url2']
+},
+{
+  name: "Osaki",
+  coordinates: {lat:33.680887, lng:130.286001},
+  maxDepth: 12,
+  description: "A lovely cove that is perfect for beginner divers.\
+    It starts out shallow with pool like conditions good for training, \
+    and then allows for divers to swim out and enjoy a relaxed dive",
+  aquaticLife: ["fish"],
+  features: ['coral', 'shallow', 'calm'],
+  pictures: ['url1', 'url2']
+}]
