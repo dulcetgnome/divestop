d1cb082db8020e5beaa37363f0ec0aba9fc4a936
Steven Lundy <steven.lundy@gmail.com>
2015-09-29 09:27:55 -0700

    (refactor) Add comments to geocoder

--- client/search/search.js
+++ client/search/search.js
@@ -4,10 +4,11 @@ angular.module('divestop.search', [])
   .controller('SearchController', function($scope, SharedProperties) {
     $scope.templateUrl = 'search/search.html';
 
+    // geocoder querys the google map api to get lat lng coordinates for an address
     var geocoder = new google.maps.Geocoder();
 
     $scope.geocodeAddress = function(address) {
-      // var address = $scope.searchBoxText;
+      // Takes in an address (String) and sets map postion
       geocoder.geocode({'address': address}, function(results, status) {
         if (status === google.maps.GeocoderStatus.OK) {
           SharedProperties.map.setCenter(results[0].geometry.location);
