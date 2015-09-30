660b678028afd1d65624ac24637192d0e662d4d8
Steven Lundy <steven.lundy@gmail.com>
2015-09-29 19:36:36 -0700

    (feat) Add photos to stub data

--- client/stub.js
+++ client/stub.js
@@ -7,8 +7,8 @@ var sites = [{
     "It starts out shallow with pool like conditions good for training, " +
     "and then allows for divers to swim out and enjoy a relaxed dive",
   aquaticLife: ["fish"],
-  features: ['coral', 'shallow', 'calm'],
-  pictures: ['url1', 'url2']
+  features: ['coral', 'shallow', 'calm','coral1', 'shallow1', 'calm1','coral2', 'shallow2', 'calm2','coral3', 'shallow3', 'calm3','coral4', 'shallow4', 'calm4'],
+  pictures: ['http://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg', 'http://dreamatico.com/data_images/cat/cat-6.jpg', 'https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg', 'http://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/articles/health_tools/foods_harmful_to_cats_slideshow/jiu_rf_photo_of_cat_licking_its_chops.jpg', 'http://rufflifechicago.com/wp-content/uploads/cat-treats.jpg', 'http://www.catprotection.com.au/wp-content/uploads/2014/11/5507692-cat-m.jpg']
 },
 {
   name: "Shirose",
@@ -19,7 +19,7 @@ var sites = [{
     "and then allows for divers to swim out and enjoy a relaxed dive",
   aquaticLife: ["fish"],
   features: ['coral', 'shallow', 'calm'],
-  pictures: ['url1', 'url2']
+  pictures: ['http://www.catsvscancer.org/wp-content/uploads/2014/10/cat-being-eaten-by-giant-fish-costume-funny.jpg', 'https://animaltrain.files.wordpress.com/2012/10/img_1199.jpg']
 },
 {
   name: "Kurose",
