252539d19744d2ef677d0fdbf15201312a462225
Garrett Maring <garrett.maring@gmail.com>
2015-09-29 17:08:10 -0700

    Travis config

--- client/stub.js
+++ client/stub.js
@@ -5,7 +5,7 @@ var sites = [{
   maxDepth: 12,
   description: "A lovely cove that is perfect for beginner divers. " +
     "It starts out shallow with pool like conditions good for training, " +
-    "and then allows for divers to swim out and enjoy a relaxed dive", "
+    "and then allows for divers to swim out and enjoy a relaxed dive",
   aquaticLife: ["fish"],
   features: ['coral', 'shallow', 'calm'],
   pictures: ['url1', 'url2']
