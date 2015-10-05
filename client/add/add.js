81b639fceae3374c1b6078a5a68a186a8bf1e3e6
Steven Lundy <steven.lundy@gmail.com>
2015-10-05 16:57:11 -0700

    Remove console statement

--- client/add/add.js
+++ client/add/add.js
@@ -100,7 +100,6 @@ angular.module('divestop.addsite', [])
   .directive('ngEnter', function () {
     return function (scope, element, attrs) {
       element.bind('keydown keypress', function (event) {
-        console.log(event.which);
         if(event.which === 13) {
           scope.$apply(function (){
             scope.$eval(attrs.ngEnter);
