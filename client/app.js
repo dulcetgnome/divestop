2996b69ab7d0b2b957282c48992a092892a8b1af
Edgar Pabon <edgar.pabon@gmail.com>
2015-10-06 21:57:01 -0700

    Added routing on client side and added views for users

--- client/app.js
+++ client/app.js
@@ -4,8 +4,56 @@
 angular.module('divestop', [
   'divestop.map',
   'divestop.divesite',
+  'divestop.users',
   'divestop.services',
   'divestop.search',
   'divestop.splash',
-  'divestop.addsite'
-]);
+  'divestop.addsite',
+  'ngRoute',
+  'satellizer'
+])
+
+.config(function ($routeProvider, $httpProvider, $authProvider) {
+  $routeProvider
+    // home page will automatically point to the map
+    .when('/', {
+      templateUrl: '/map/map.html',
+      controller: 'OurMapController'
+    })
+    .when('/signup', {
+      templateUrl: '/users/signup.html',
+      controller: 'userController'
+    })
+    .when('/login', {
+      templateUrl: '/users/login.html',
+      controller: 'userController'
+    })
+    .when('/profile', {
+      templateUrl: '/users/profile.html',
+      controller: 'userController'
+    })
+    .when('/settings', {
+      templateUrl: '/users/settings.html',
+      controller: 'userController'
+    })
+    .when('/bars', {
+      templateUrl: '/divesite/divesite.html',
+      controller: 'DiveSiteController'
+    })
+    .when('/bars/new', {
+      templateUrl: '/add/add.html',
+      controller: 'AddSiteController'
+    })
+    .otherwise('/add/add.html');
+
+  // use satellizer for user authentication
+  $authProvider.facebook({
+    clientId: 'Facebook App ID'
+  });
+
+  $authProvider.google({
+    clientId: 'Google Client ID'
+  });
+
+});
+
