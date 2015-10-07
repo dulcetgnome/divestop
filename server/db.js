09dad3905ae493a5317c6e0c209fb75aa55c3f33
tscheys <tim.scheys@gmail.com>
2015-10-07 16:24:04 -0700

    refactor params variable

--- server/db.js
+++ server/db.js
@@ -232,11 +232,15 @@ exports.search = function(cb, passedLocation) {
   // passedlocation is array of long and latitude
   // should be integers
   var locationQuery = '';
-  var params = [];
+  var upperLat = passedlocation[0] - 1;
+  var lowerLat = passedlocation[0] + 1;
+  var upperLong = passedlocation[1] - 1;
+  var lowerLong = passedlocation[1] + 1;
+  var params = [lowerLat, upperLat, lowerLong, upperLong];
   if (passedLocation) {
     params = passedlocation
     // need to get lat and long from search 
-    locationQuery = ' WHERE l.lat BETWEEN ($1 - 10) AND ($1 - 10) AND l.long BETWEEN ($1 + 10) AND ($2 - 10)';
+    locationQuery = ' WHERE l.lat BETWEEN $1 AND $2 AND l.long BETWEEN $3 AND $4';
   }
 
   var queryString = 'SELECT s.site, l.location, s.lat, s.long, s.max_depth, ' + 
