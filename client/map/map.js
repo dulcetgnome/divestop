8f50ed3e7244fd47474cc5d929e2faec31ff5628
tksander <tkepasander@gmail.com>
2015-10-08 21:45:31 -0700

    Working on map toggle

--- client/map/map.js
+++ client/map/map.js
@@ -19,8 +19,9 @@ angular.module('divestop.map', ['ngMap'])
       AppMap.getMap(map);
     });
 
-
     $scope.toggleForm = function() {
+      var toggle = true;
+      console.log(toggle);
       $scope.showForm.state = !$scope.showForm.state;
       if($scope.showForm.state) {
         AppMap.showNewMarker();
